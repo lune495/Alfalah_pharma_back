fe47051a690dda3e7b38888320e43838eff860ea
lune495 <badaralune9@gmail.com>
2023-11-14 01:50:51 +0000

    Update

--- config/graphql.php
+++ config/graphql.php
@@ -117,7 +117,7 @@ return [
             ],
 
             // Laravel HTTP middleware
-            'middleware' => ['auth:sanctum'],
+            'middleware' => ['web'],
 
             // Which HTTP methods to support; must be given in UPPERCASE!
             'method' => ['GET', 'POST'],
